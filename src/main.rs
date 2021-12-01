46ade416dc26a1b7ef6e938c81a1ccdd019fafae
NO ONE <nope@example.com>
2021-12-01 17:41:32 -0600

    - la la la

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::io::{stdin};
+
 fn main() {
-    println!("Hello, world!");
+    let mut infile_path = String::new();
+
+    println!("Path to input file: ");
+    stdin().read_line(&mut &mut infile_path).expect("Apparently you are bad at typing?  Somehow?");
+
 }
+
