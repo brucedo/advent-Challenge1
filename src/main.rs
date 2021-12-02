9760d28b5db0dbdf0c077cb9caa10c94a5074485
Doug Bruce <queazan@gmail.com>
2021-12-01 22:16:32 -0600

    - Challenge 2 complete and won.  As a personal note, the leaderboard idea is not great, given how much of an advantage it gives people in specific time zones.  If anyone running the calendar ever reads this....

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,101 @@
-use std::{io::{stdin, BufRead, BufReader}, path::Path, fs::File, process::exit};
+use std::{io::{stdin, BufRead, BufReader, Seek, SeekFrom}, path::Path, fs::File, process::exit};
 
 
 fn main() 
 {
     let mut reader = get_reader();
 
+    let increase_result = count_increases(&mut reader);
+
+    if increase_result.is_err()
+    {
+        println!("{}", increase_result.unwrap_err());
+    }
+    else
+    {
+        let count = increase_result.unwrap();
+        println!("Total number of increases: {}.", count);
+    }
+
+    reader.seek(SeekFrom::Start(0));
+
+    let rolling_increase_result = count_rolling_increases(&mut reader);
+    if rolling_increase_result.is_err()
+    {
+        println!("{}", rolling_increase_result.unwrap_err());
+    }
+    else
+    {
+        let rolling_count = rolling_increase_result.unwrap();
+        println!("Total number of rolling window increases: {}", rolling_count);
+    }
+}
+
+fn count_rolling_increases(reader: &mut BufReader<File>) -> Result<i32, String>
+{
+    // initialize our window
+    let mut sum_window: [i32; 3] = [0, 0, 0];
+    let mut first = -1;
+    let mut second = -1;
+    let mut k = 0;
+
+    let mut count = 0;
+
+    // Prime the pump...
+    let mut next_num = read_int_from_file(reader)?;
+    sum_window[0] += next_num;
+
+    next_num = read_int_from_file(reader)?;
+    sum_window[0] += next_num;
+    sum_window[1] += next_num;
+
+    next_num = read_int_from_file(reader)?;
+
+
+    while next_num > -1 {
+
+        sum_window[0] += next_num;
+        sum_window[1] += next_num;
+        sum_window[2] += next_num;
+
+        first = second;
+        second = sum_window[k];
+        sum_window[k] = 0;
+
+        k = (k + 1) % 3;
+
+        if first > -1 && second > -1
+        {
+            if first < second
+            {
+                count += 1;
+            }
+        }
+
+        next_num = read_int_from_file(reader)?;
+    }
+
+    return Ok(count);
+}
+
+fn count_increases(reader: &mut BufReader<File>) -> Result<i32, String>
+{
     let mut count: i32 = 0;
 
-    let mut last_num = read_int_from_file(&mut reader);
+    let mut last_num = read_int_from_file(reader);
 
     if last_num.is_err()
     {
-        println!("Something went wrong during the read in.");
-        exit(-1);
+        return Err("Something went wrong during the read in.".to_string());
     }
 
     let mut first_num = last_num.unwrap();
     
-    last_num = read_int_from_file(&mut reader);
+    last_num = read_int_from_file(reader);
     if last_num.is_err()
     {
-        println!("Something went wrong during the read in.");
-        exit(-1);
+        return Err("Something went wrong during the read in.".to_string());
+        
     }
     let mut second_num = last_num.unwrap();
 
@@ -33,16 +107,16 @@ fn main()
         }
         first_num = second_num;
 
-        last_num = read_int_from_file(&mut reader);
+        last_num = read_int_from_file(reader);
         if last_num.is_err()
         {
-        println!("Something went wrong during the read in.");
-        exit(-1);
+            return Err("Something went wrong during the read in.".to_string());
+        
         }
         second_num = last_num.unwrap();
     }
 
-    println!("Total number of increases: {}.", count);
+    return Ok(count);
 }
 
 fn read_int_from_file(reader:&mut BufReader<File>) -> Result<i32, String>
@@ -112,7 +186,6 @@ fn get_reader() -> BufReader<File>
 
         open_result = File::open(path);
     }
-
     BufReader::new(open_result.unwrap())
 }
 
